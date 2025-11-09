194792a30965d6d3f4b46a6fc4a7c8987803beb8
segun651 <omojolasegun222@gmail.com>
2025-11-10 00:36:12 +0100

    feat: login page with Formik and Yup validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,26 @@
+import styles from './Login.module.scss'
+import CompanyLogo from "../../assets/images/Group.png"
+import LoginImage from "../../assets/images/pablo-sign-in 1.png"
+import LoginForm from './LoginForm'
 
 export default function Login() {
   return (
-    <div>Login Page</div>
+    <div className={styles.container}>
+
+<img src={CompanyLogo} alt="Company Logo" className={styles.loginImage} loading='lazy'/>
+
+<div className={styles.loginBox}>
+   <img src={LoginImage} alt="LoginImage" />
+  <section>
+    <div className={styles.loginHeader}>
+<h1>Welcome!</h1>
+     <p> Enter details to login.</p>
+    </div>
+     
+     <LoginForm />
+  </section>
+</div>
+
+    </div>
   )
 }
---
+++ src/pages/Login/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import * as Yup from "yup"
+import styles from "./Login.module.scss"
+import { useState } from "react";
+
+const LoginSchema = Yup.object({
+
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().min(6, "Too short").required("Password is required"),
+});
+
+export default function LoginForm() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  function handleSubmit() {
+       localStorage.setItem("isAuthenticated", "true");
+      window.location.reload();
+    // Submission logic can be added here
+  }
+ function handleForgot() {
+    alert("Password reset link has been sent to your email.");
+  }
+  
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={LoginSchema}
+      onSubmit={(values) => {
+     handleSubmit();
+      
+      }}
+    >
+      <Form className={styles.loginForm}>
+        <div>
+        
+<Field name="email" type="email" placeholder="Email"  className={styles.field}/>
+          <ErrorMessage name="email" component="div" className={styles.error} />
+        </div>
+
+        <div>
+          <div className={styles.emailField}>
+          <Field name="password" type={showPassword ? "text" : "password"} placeholder="Password" className={styles.field} />
+          <span onClick={()=> setShowPassword(!showPassword)}>{showPassword ? "Hide" : "Show"}</span>
+          </div>
+          <ErrorMessage name="password" component="div" className={styles.error} />
+        </div>
+
+      <a className={styles.forgotBtn} onClick={handleForgot}>Forgot Password ?</a>
+    
+        <button type="submit">Log In</button>
+      </Form>
+    </Formik>
+  );
+}
+
